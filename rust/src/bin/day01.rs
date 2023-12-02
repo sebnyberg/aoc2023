5f767e6fac611184254b3a69fdedf8711b70ea0d
Sebastian Nyberg <seb@spaced.local>
2023-12-02 22:00:30 +0100

    start toying with io

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    // List cwd
+    let cwd = std::env::current_dir().unwrap();
+    println!("cwd: {:?}", cwd);
+
+    // Open the file
+    let file = File::open("src/bin/day01input").expect("Could not open file");
+
+    // let reader = io::BufReader::new(file);
+
+    println!("Hello, world1");
 }
