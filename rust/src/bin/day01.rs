76ba77a853120357bc596fa786d5050e792aed1b
Sebastian Nyberg <seb@spaced.local>
2023-12-03 11:55:33 +0100

    day01

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -1,16 +1,9 @@
-use std::fs;
-use std::fs::File;
-use std::io::{self, BufRead};
+mod day01 {
+    pub fn part1() {
+        println!("Hello, world!");
+    }
+}
 
 fn main() {
-    // List cwd
-    let cwd = std::env::current_dir().unwrap();
-    println!("cwd: {:?}", cwd);
-
-    // Open the file
-    let file = File::open("src/bin/day01input").expect("Could not open file");
-
-    // let reader = io::BufReader::new(file);
-
-    println!("Hello, world1");
+    day01::part1();
 }
